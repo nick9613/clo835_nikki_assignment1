3599c7d2f4e930576a366ec1ef024d905c7740df
kenoah <kenoah1998@gmail.com>
2023-06-04 02:35:17 +0000

    Adding my commits

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Kennith Noah"
+    "App"   = "COLORAPP"
+    "Project" = "Assignment1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "kennith"
+}
